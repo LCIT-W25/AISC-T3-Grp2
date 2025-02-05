7e3ef7bbc16fef9a8fab08886dde64334d7b5312
Prakash Pun <poonprakash22@gmail.com>
2025-02-04 19:24:01 -0500

    rename file Assignment 1 to assignment_1

--- AISC2013-Deployment/Assignment 1/prakash_image_class_ui/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-class Config:
-    # Set via env variable in production
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'alongsecretekeyhahahah')
-    FLASK_ENV = os.environ.get('FLASK_ENV', 'development')
-    UPLOAD_FOLDER = 'app/static/uploads'
-    ALLOWED_EXTENSIONS = {'txt', 'csv', '.png', '.jpg', '.jpeg'}
